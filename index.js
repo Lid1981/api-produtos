73aee4fe097cc6382498c703e3df5b61a0729297
Lid1981 <lidramos1981@gmail.com>
2025-06-02 11:54:31 -0300

    Criando rotas

--- index.js
+++ index.js
@@ -1,54 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const produtosDb = []
-const usersDb = []
+const port = 3000;
+const produtosRoutes = require('./src/routes/produtos')
 
 app.use(express.json())
+app.use(produtosRoutes)
 
-    app.get('/', (req, res) => {
-        res.send('Olá de novo!')
-  })
 
-    app.post("/produtos", (req, res) => {
-        const {nome, preco, categoria} = req.body
 
-        
-        if(!nome || !preco || !categoria){
-            return res.status(400).send("Nome, preço e categoria são obrigatórios!")
-        }
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
 
-        
-        
-        const produto = {
-            nome: nome,
-            preco: preco,
-            categoria: categoria
-        }
-
-        produtosDb.push(produto)
-
-        res.status(201).send(produto)
-    })
-
-    app.get("/produtos", (req, res) => {
-        res.send(produtosDb)
-    })
-
-    app.post("/usuarios", (req, res) => {
-        const {nome, email, senha} = req.body
-
-        if(!nome || !email || !senha){
-            return res.status(400).send("Nome, email e senha são obrigatórios!")
-        }
-        
-        usersDb.push({nome, email, senha})
-
-        res.status(201).send("Usuário cadastrado com sucesso!")
-
-    })
-
-    app.listen(port, () => {
-        console.log(`O servidor está rodando na porta ${port}`)
-  })
 
---
+++ src/routes/produtos.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const produtosController = require('../controllers/produtos')
+
+router.get('/produtos', produtosController.getProdutos)
+router.post('/produtos', produtosController.createProduto)
+
+module.exports = router;
+
